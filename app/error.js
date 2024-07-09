f00a5aff146c162fc206ab61887abbf2fde3c6d1
3nonch <enonch@hotmail.com>
2024-07-09 14:13:15 +0300

    error pages & loading component

---
+++ app/error.js
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import { Router } from "next/router";
+
+export default function Error({ error, reset }) {
+  return (
+    <main className="flex justify-center items-center flex-col gap-6">
+      <h1 className="text-3xl font-semibold">Something went wrong!</h1>
+      <p className="text-lg">{error.message}</p>
+
+      <button
+        onClick={reset}
+        className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
+      >
+        Try again
+      </button>
+      <span>OR</span>
+      <Link
+        href="/"
+        className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
+      >
+        Go back home
+      </Link>
+    </main>
+  );
+}
